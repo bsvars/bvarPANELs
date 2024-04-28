74b18d3154899c0567066740f2e8ed2ab4e4c912
Tomasz Woźniak <wozniak.tom@pm.me>
2024-04-28 22:37:23 +1000

    created sample_m #2

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -25,9 +25,25 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// sample_m
+double sample_m(const arma::mat& aux_A, const arma::mat& aux_V, const double& aux_s, const double& aux_w, const Rcpp::List& aux_prior);
+RcppExport SEXP _bvarPANELs_sample_m(SEXP aux_ASEXP, SEXP aux_VSEXP, SEXP aux_sSEXP, SEXP aux_wSEXP, SEXP aux_priorSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const arma::mat& >::type aux_A(aux_ASEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type aux_V(aux_VSEXP);
+    Rcpp::traits::input_parameter< const double& >::type aux_s(aux_sSEXP);
+    Rcpp::traits::input_parameter< const double& >::type aux_w(aux_wSEXP);
+    Rcpp::traits::input_parameter< const Rcpp::List& >::type aux_prior(aux_priorSEXP);
+    rcpp_result_gen = Rcpp::wrap(sample_m(aux_A, aux_V, aux_s, aux_w, aux_prior));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_bvarPANELs_rmniw1", (DL_FUNC) &_bvarPANELs_rmniw1, 4},
+    {"_bvarPANELs_sample_m", (DL_FUNC) &_bvarPANELs_sample_m, 5},
     {NULL, NULL, 0}
 };
 
--- src/sample_mniw.cpp
+++ src/sample_mniw.cpp
@@ -22,3 +22,33 @@ arma::field<arma::mat> rmniw1(
   out(1)        = Sigma;
   return out;
 } // END rmniw1
+
+
+
+// [[Rcpp::export]]
+double sample_m (
+    const arma::mat&    aux_A,    // KxN
+    const arma::mat&    aux_V,    // KxK
+    const double&       aux_s,   // scalar
+    const double&       aux_w,   // scalar
+    const Rcpp::List&   prior
+) {
+  
+  const int N       = aux_A.n_cols;
+  mat prior_S_inv   = as<mat>(prior["S_inv"]);
+  mat prior_S       = inv_sympd(prior_S_inv);
+  double prior_mu_m = as<double>(prior["mu_m"]);
+  double prior_sigma2_m = as<double>(prior["sigma2_m"]);
+  
+  double precision_tmp  = (1 / prior_sigma2_m);
+  double mean_tmp       = (prior_mu_m / prior_sigma2_m);
+  for (int n = 0; n < N; n++) {
+    precision_tmp  += 1 / (aux_s * aux_w * prior_S(n, n) * aux_V(n, n));
+    mean_tmp       += aux_A(n, n) / (aux_s * aux_w * prior_S(n, n) * aux_V(n, n));
+  }
+  double sigma2_m_bar   = 1 / precision_tmp;
+  double mu_m_bar       = sigma2_m_bar * mean_tmp;
+  
+  double out        = randn( distr_param(mu_m_bar, pow(sigma2_m_bar, 0.5)) );
+  return out;
+} // END sample_m
