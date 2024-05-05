f5e7723bb1a96d0186a980e75274ef999228ae7b
Tomasz Woźniak <wozniak.tom@pm.me>
2024-05-05 19:41:13 +1000

    included ordinal in utils #2

---
+++ src/utils.h
@@ -0,0 +1,15 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <RcppArmadillo.h>
+
+using namespace Rcpp;
+using namespace arma;
+
+
+std::string ordinal(
+    int n
+);
+
+
+#endif  // _UTILS_H_
