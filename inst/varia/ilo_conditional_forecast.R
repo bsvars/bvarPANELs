0249122ecb841a8cde9035832d8c0f1e6bdf588b
Tomasz Woźniak <wozniak.tom@pm.me>
2024-06-06 22:23:35 +1000

    create ilo_conditional_forecast file #3

---
+++ inst/varia/ilo_conditional_forecast.R
@@ -0,0 +1,26 @@
+
+# Create ILO conditional forecasts from provided files
+library(dplyr)
+
+# this file contains a cubic panel dynamic dataset
+all_cv    <- read.csv("inst/varia/ilo_cubic_panel.csv")
+colnames(all_cv) = c("year", "iso3code", "country", "UR", "EPR", "LFPR", "dgdp")
+
+# all variables all countries
+data_cv <- all_cv %>% 
+  filter(year >= 2024) %>% 
+  select(year, iso3code, UR, EPR, LFPR, dgdp)
+
+# Create a list with the country data
+countries = unique(data_cv$iso3code)
+countries = countries[order(countries)]
+ilo_conditional_forecast = list()
+for (i in 1:length(countries)) {
+  ilo_conditional_forecast[[i]] <- data_cv %>% 
+    filter(iso3code == countries[i]) %>% 
+    select(UR, EPR, LFPR, dgdp) %>% 
+    ts(start = 2024, frequency = 1)
+  names(ilo_conditional_forecast)[i] <- countries[i]
+}
+
+save(ilo_conditional_forecast, file = "data/ilo_conditional_forecast.rda")
