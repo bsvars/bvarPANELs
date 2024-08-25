fc65b19db5c8778ca5bc30b48934fe3d7ec46d3f
Tomasz Woźniak <wozniak.tom@pm.me>
2024-08-25 23:58:48 +1000

    update of the bvarPANELs doc

--- R/bvarPANELs-package.R
+++ R/bvarPANELs-package.R
@@ -26,6 +26,115 @@
 #' Bayesian Vector Autoregressions with a three-level country-global 
 #' hierarchical prior structure. Copyright: 2024 International Labour Organization.
 #' 
+#' @details 
+#' The package provides a set of functions for predictive analysis with the 
+#' Bayesian Hierarchical Panel Vector Autoregression. The model specification
+#' is initiated using function \code{\link{specify_bvarPANEL}} that creates 
+#' an object of class \code{BVARPANEL} containing the prior specification, 
+#' starting values for estimation, data matrices, and some parameters of the 
+#' Monte Carlo Markov Chain sampling algorithm.
+#' 
+#' \strong{The Model.} The model features country-specific Vector Autoregressive 
+#' (VAR) equation for \code{N} dependent variables with \code{T_c} observations 
+#' for each country \code{c}. Its equation is given by
+#' \deqn{\mathbf{Y}_c = \mathbf{A}_c\mathbf{X}_c + \mathbf{E}_c}
+#' where \eqn{\mathbf{Y}_c} is an \code{T_c x N} matrix of dependent variables for 
+#' country \code{c}, \eqn{\mathbf{X}_c} is a \code{T_c x K} matrix of explanatory 
+#' variables, \eqn{\mathbf{E}_c} is an \code{T_c x N} matrix of error terms, and 
+#' \eqn{\mathbf{A}_c} is an \code{NxK} matrix of country-specific autoregressive slope 
+#' coefficients and parameters on deterministic terms in \eqn{\mathbf{X}_c}. The 
+#' parameter matrix \eqn{\mathbf{A}_c} includes autoregressive matrices capturing the 
+#' effects of the lagged vectors of dependent variables at lags from 1 to \code{p},
+#' a constant term and a set of exogenous variables.
+#' 
+#' The error terms for each of the periods have zero conditional mean, 
+#' conditional covariance given by the \code{NxN} matrix \eqn{\mathbf{\Sigma}}. The errors 
+#' are jointly normally distributed and not serially correlated. These 
+#' assumptions are summarised using a matrix-variate normal distribution 
+#' (see Woźniak, 2016):
+#' \deqn{\mathbf{E}_c \sim MN(\mathbf{0}_{T_c x N}, \mathbf{\Sigma}, \mathbf{I}_{T_c})}
+#' where the identity matrix \eqn{\mathbf{I}_{T_c}} and joint normality imply no serial
+#' autocorrelation.
+#' 
+#' \strong{Global Prior Distributions.} 
+#' The Hierarchical Panel VAR model features a sophisticated hierarchical prior
+#' structure that grants the model flexibility, interpretability, and improved 
+#' forecasting performance.
+#' 
+#' The country-specific parameters follow a prior distribution that, at its 
+#' mean value, represents a global VAR model with a global autoregressive 
+#' parameter matrix \eqn{\mathbf{A}} of dimension \code{KxN} and an \code{NxN} global 
+#' covariance matrix \eqn{\mathbf{\Sigma}}:
+#' \deqn{\mathbf{Y}_c = \mathbf{AX}_c + \mathbf{E}_c}
+#' This global VAR model under the prior mean is represented by the parameters 
+#' of the matrix-variate normal inverted Wishart distribution (see Woźniak, 2016)
+#' given by:
+#' \deqn{\mathbf{A}_c, \mathbf{\Sigma}_c | \mathbf{A}, \mathbf{V}, \mathbf{\Sigma}, \nu \sim MNIW(\mathbf{A}, \mathbf{V}, (N - \nu - 1)\mathbf{\Sigma}, \nu)}
+#' where \eqn{V} is a \code{KxK} column-specific covariance matrix, 
+#' \eqn{(N - \nu - 1)\mathbf{\Sigma}} is the row-specific matrix, and \eqn{\nu} is the 
+#' degrees-of-freedom parameter. 
+#' 
+#' All of the parameters of the prior distribution above feature their own prior
+#' distributions and are estimated. These prior distributions are given by:
+#' \deqn{\mathbf{A} | \mathbf{V}, m, w, s \sim MN (m\underline{\mathbf{M}}, \mathbf{V}, s\underline{\mathbf{S}} ) }
+#' with the \code{KxN} mean matrix \eqn{m\underline{\mathbf{M}}}, the \code{KxK}
+#' column-specific covariance matrix \eqn{\mathbf{V}}, and the \code{NxN} matrix of
+#' row-specific covariance \eqn{s\underline{\mathbf{S}}}.
+#' 
+#' The global error term covariance matrix, \eqn{\mathbf{\Sigma}}, follows a Wishart 
+#' distribution with \code{NxN} scale matrix \eqn{s\underline{\mathbf{S}}_\Sigma} 
+#' and shape parameter \eqn{\underline{\mu}_\Sigma}
+#' \deqn{\mathbf{\Sigma} | s, \nu \sim W(s\underline{\mathbf{S}}_\Sigma,\underline{\mu}_\Sigma)}
+#' 
+#' \strong{Other Prior Distributions.} 
+#' The column-specific covariance \eqn{\mathbf{V}} follows the inverse-Wishart 
+#' distribution with scale \eqn{w\underline{\mathbf{W}}} and shape 
+#' \eqn{\underline{\eta}}:
+#' \deqn{\mathbf{V} | w \sim IW(w\underline{\mathbf{W}}, \underline{\eta})}
+#' The shape parameter \eqn{\nu} follows an exponential distribution with mean 
+#' \eqn{\underline\lambda}:
+#' \deqn{\nu \sim\exp(\underline\lambda)}
+#' Finally, the priors for the remaining scalar hyper-parameters are:
+#' \deqn{m \sim N(\underline{\mu}_m, \underline{\sigma}_m^2)}
+#' \deqn{w \sim G(\underline{s}_w, \underline{a}_w)}
+#' \deqn{s \sim IG2 (\underline{s}_s, \underline{\nu}_s)}
+#' 
+#' \strong{Estimation.}
+#' The package implements Bayesian estimation using the Gibbs sampler. This 
+#' sampling algorithm provides a sample of random draws from the posterior 
+#' parameters of the model defined by Bayes' Rule stating that the posterior
+#' distribution of the parameters given data is proportional to the likelihood 
+#' function and the prior distribution of the parameters:
+#' \deqn{p(\mathbf{\theta} | \mathbf{Y}) \propto L(\mathbf{\theta}; \mathbf{Y}) p(\mathbf{\theta})}
+#' where \eqn{\mathbf{\theta}} collects all the parameters of the model.
+#' At each of its iterations a single draw of all of the 
+#' parameters of the model, including the estimated hyper-parameters, is obtained. 
+#' This Bayesian procedure estimates jointly all the parameters of the model and
+#' is implemented in the \code{\link{estimate.BVARPANEL}} and 
+#' \code{\link{estimate.PosteriorBVARPANEL}} functions.
+#' 
+#' \strong{Forecasting.} 
+#' The package implements Bayesian forecasting providing the a sample of draws
+#' from the joint predictive density defined as the joint density of the future
+#' unknown values to be predicted, \eqn{\mathbf{Y}_f}, given data, \eqn{\mathbf{Y}}
+#' closely following Karlsson (2013):
+#' \deqn{p(\mathbf{Y}_f | \mathbf{Y})}
+#' The package offers the possibility of conditional predictions given provided 
+#' future projections for some of the variables. The forecasting is performed 
+#' using function \code{\link{forecast.PosteriorBVARPANEL}}.
+#' 
+#' @seealso \code{\link{specify_bvarPANEL}}, \code{\link{estimate.BVARPANEL}}, 
+#' \code{\link{forecast.PosteriorBVARPANEL}}
+#' 
+#' @references
+#' Karlsson, S. (2013). Forecasting with Bayesian Vector Autoregression, 
+#' in: \emph{Handbook of Economic Forecasting}, Elsevier. volume \bold{2}, 791–897,
+#' \doi{10.1016/B978-0-444-62731-5.00015-4}.
+#' 
+#' Woźniak, T. (2016). Bayesian Vector Autoregressions, 
+#' \emph{Australian Economic Review}, \bold{49}, 365-380, 
+#' \doi{10.1111/1467-8462.12179}.
+#' 
 #' @name bvarPANELs-package
 #' @aliases bvarPANELs-package bvarPANELs
 #' @docType package
@@ -39,7 +148,6 @@
 #' @note This package is currently in active development.
 #' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
 #' @keywords package models ts
-#' #' @examples
 #' @examples
 #' # Basic estimation and forecasting example
 #' ############################################################
@@ -58,6 +166,8 @@
 #'   estimate(S = 20) |> 
 #'   forecast(horizon = 2) -> predictive
 #'   
+#' plot(predictive, which_c = "POL")
+#' 
 #' # Full estimation and forecasting example with 
 #' #   exogenous variables and conditional forecasts
 #' ############################################################
@@ -78,4 +188,7 @@
 #'                   exogenous_forecast = ilo_exogenous_forecasts,
 #'                   conditional_forecast = ilo_conditional_forecasts
 #'                 )
+#'                 
+#' plot(predictive, which_c = "POL")
+#' 
 "_PACKAGE"
